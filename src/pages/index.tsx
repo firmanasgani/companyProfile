8eee34d76df1cbe8b0f575864bf0eb2441f74aa9
firman maulana <firmanasgani37@gmail.com>
2024-07-07 03:13:59 +0700

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import Content from "@/Components/Content";
 import Header from "@/Components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Home | CITG</title>
+        <meta name="description" content="Part of absurdity" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo-CITG.webp" />
+      </Head>
       <Header />
       <Content />
     </main>
