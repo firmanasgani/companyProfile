c335620855dc8771d5496831fcec59f0475c4e07
firman maulana <firmanasgani37@gmail.com>
2024-07-07 02:40:56 +0700

    set header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,9 +7,12 @@ export default function Header() {
         <a href="#ceritanya">Ceritanya</a>
         <a href="#about">About</a>
       </div>
-      <div style={{ padding: "20px" }} className="banner"> 
+      <div style={{ padding: "20px" }} className="banner">
         <h1>Welcome to Our Website</h1>
-        <p>We are absurd in our way</p>
+        <p className="text-title">We are absurd in our way</p>
+        <p style={{marginTop: "20px"}}>
+          <a href="https://www.youtube.com/channel/UCjtsahDG-ST5nxvq4iroXlw" target="_blank" className="btn-cta">Visit our channel</a>
+        </p>
       </div>
     </header>
   );
