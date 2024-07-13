1e7e142c5d3f8eb729619fc960c84fdf2a89963c
firmanasgani <firmnasgani37@gmail.com>
2024-07-13 21:07:22 +0700

    change to bun

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <link rel="icon" href="/logo-CITG.webp" />
       </Head>
       <Header />
-      {/* <Carousels /> */}
       <Content />
       <Footer />
     </main>
